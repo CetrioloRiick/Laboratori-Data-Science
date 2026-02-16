8901544eeb0a170af474e98fa06629f7ed3f3f16
CetrioloRiick <diego.q@ik.me>
2026-02-16 16:59:59 +0000

    sjadijkdafkfjad

--- 3/3 - EigenFace/cacca.py
+++ 3/3 - EigenFace/cacca.py
@@ -1,33 +1,74 @@
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
+import joblib
+
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
+def facesProcessing(data):
+    faces = mat_contents["faces"].T
+    faces_2d = faces.reshape(-1, 168, 192)   # (N,168,192)
+    faces_rotate = faces_2d.transpose(0, 2, 1)  # (N,192,168)
+    faces_final = faces_rotate.reshape(len(faces), -1)
+    return faces_final
 
+fc_shape = (192, 168)
 mat_contents = scipy.io.loadmat("3/3 - EigenFace/allFaces.mat")
-faces = mat_contents["faces"].T
+faces = facesProcessing(mat_contents)
 
+plt.imshow(faces[0].reshape(fc_shape))
+plt.show()
+
+""" 
+
+ 
 st_instance = StandardScaler()
 faces_scaled = st_instance.fit_transform(faces)
 
-ca_instance = PCA(n_components=50, random_state=42)
-faces_transformed = pca_instance.fit_transform(faces_scaled)
 
-while True:
-    i = int(input("primo ciclo quante componenti vuoi fare la pca: "))
-    pca_instance = PCA(n_components=i, random_state=42)
-    faces_transformed = pca_instance.fit_transform(faces_scaled)
-    fig, axes = plt.subplots(2, 1, figsize=(10, 8), constrained_layout=True)
-    fig.suptitle(f"Originale vs Ricostruzione", fontsize=12)
-    n = int(input("numerino fino a 10.000"))
-    faces_inverse = pca_instance.inverse_transform(faces_transformed)
-    X_sample = faces_inverse[n]
-    axes[0].imshow(np.reshape(X_sample, shape=(168, 192)), cmap="gray")
-    # print("questo era un:", y_test.iloc[[n]])
-    # print("secondo il modello era un:", pippo.predict(X_test.iloc[[n]]))
-    axes[1].imshow(np.reshape(faces_scaled[n], (168, 192)), cmap="gray")
+# CARICAAMENTO DEL MODELLO
+path_pca = "3/3 - EigenFace/pca.pkl"
+if os.path.exists(path_pca):
+    print(f"Caricamento modello PCA...\n")
+    pca = joblib.load(path_pca)
+
+else:
+    pca = PCA(random_state=42)
+
+    print("Addestramento PCA...")
+    pca.fit(X_train)
+    print("Addestramento completato.\n")
+
+    # Salvataggio modello
+    joblib.dump(pca, path_pca)
+    print(f"Modello salvato in '{path_pca}'.")
+
+mean_face = pca.mean_.reshape(m, n).T  # sklearn salva cosi la faccia media
+eigenfaces = pca.components_  # lista con tutte le autofacce
+# Visualizzo le auto facce una alla volta, iniziamo con le prime 10
+fig, axes = plt.subplots(2, 5, figsize=(10, 8), constrained_layout=True)
+axes = axes.ravel()
+for i, ax in enumerate(axes):
+
+    eigenFace = pca.components_[i]
+    image = np.reshape(eigenFace, shape=(168, 192))
+    ax.imshow(image, cmap="gray")
+plt.show()
+
+
+img = plt.imread("3/3 - EigenFace/foto.png").T.ravel()
+print(img)
+plt.imshow(np.reshape(img, shape=(168, 192)), cmap="gray")
+plt.show()
+
+k = np.linspace(10, 50, dtype=int, num=5)
+for i in k:
+    img_transformed = pca.transform([img])
+    img_transformed[:, i:] = 0
+    img_reverse = pca.inverse_transform(img_transformed)
+    plt.imshow(np.reshape(img_reverse[0], shape=(168, 192)), cmap="gray")
     plt.show()
 
-
 print("ho finito cazzoni")
+ """
